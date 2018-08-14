ae5bfde25837f07143cb3f7ebf84364da5be3537
Robert Zehnder <robert@kisdigital.com>
2018-08-14 13:19:44 -0400

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,56 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+    <v-app light>
+        <v-navigation-drawer persistent :mini-variant="miniVariant" :clipped="clipped" v-model="drawer" enable-resize-watcher fixed
+            app>
+            <v-list>
+                <v-list-tile router v-for="(item, i) in items" :to="item.to" :key="i">
+                    <v-list-tile-action>
+                        <v-icon v-html="item.icon"></v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title v-text="item.title"></v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar app :clipped-left="clipped">
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+        </v-content>
+        <v-footer :fixed="fixed" app>
+            <span>&copy; 2017</span>
+        </v-footer>
+    </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire',
+        to: '/'
+      },
+      {
+        icon: 'bubble_chart',
+        title: 'About',
+        to: '/about'
+      }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
     }
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+            <v-layout column align-center>
+                <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
+                <blockquote>
+                    &#8220;First, solve the problem. Then, write the code.&#8221;
+                    <footer>
+                        <small>
+                            <em>&mdash;John Johnson</em>
+                        </small>
+                    </footer>
+                </blockquote>
+            </v-layout>
+        </v-slide-y-transition>
+    </v-container>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
-</script>
